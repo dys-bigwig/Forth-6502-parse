a37be5c6cb92e51712909b18de884fb2abad4004
dys-bigwig <45558889+dys-bigwig@users.noreply.github.com>
2019-08-15 12:15:41 +0100

    Update asm.fs

--- asm.fs
+++ asm.fs
@@ -8,51 +8,143 @@
 \           $1234 LDA    -- absolute
 \           $2A (,X) LDA -- indexed indirect
 \           $2A (,Y) LDA -- indirect indexed
-\ -----------------------------------------------------------------------
+\ \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+
+( UTILITIES ) \ ------------------------------------------------------------------------------
+
+: @INDEX ( uindex - copcode ) CELLS + @ ;
+
+: INT->BYTE ( n -- b )   SP@ C@ SWAP DROP ;
+
+: ?<=$FFFF ( operand -- finrange )
+    $FFFF <= ;
+
+: ?>=-32768 ( operand -- finrange )
+   -32768 >= ;
+
+: ?IN-SIGNED-8BIT-RANGE
+    DUP -128 >= SWAP $FF <= AND ;
+
+: ?OPERAND-IN-ZP-RANGE ( operand -- finrange )     \ distinguishes between zp and absolute addressing modes
+    ?IN-SIGNED-8BIT-RANGE ;
+
+: ?OPERAND-IN-IMM-RANGE ( operand -- finrange )    \ registers can only hold 8 bits
+    ?IN-SIGNED-8BIT-RANGE ;
+
+( ADDRESSING MODE WORDS ) \ ------------------------------------------------------------------
+
+: ?VALID-MODE @INDEX 0= INVERT ;
+
+: MODE-DEFAULT ( noperand caddr -- coperand copcode )
+    SWAP DUP
+      ?OPERAND-IN-ZP-RANGE
+        IF SWAP DUP 2 ?VALID-MODE
+                        IF 2 @INDEX
+                        ELSE ." ZP INVALID" ENDIF
+        ELSE SWAP DUP 5 ?VALID-MODE
+                          IF 5 @INDEX
+                          ELSE ." ABS INVALID" ENDIF
+        ENDIF ;
+
+: MODE-IMM
+    SWAP DUP ?OPERAND-IN-IMM-RANGE
+               IF SWAP 1 @INDEX
+               ELSE . ." OUT OF BOUNDS FOR IMM ADDR MODE" DROP ENDIF ;
+
+: MODE-ZP-X SWAP DUP
+              ?OPERAND-IN-ZP-RANGE
+                IF SWAP 3 @INDEX 
+                ELSE . ." OUT OF BOUNDS FOR ZP,X ADDR MODE " ENDIF ;
+
+: MODE-ZP-Y SWAP DUP
+              ?OPERAND-IN-ZP-RANGE
+                IF SWAP 4 @INDEX 
+                ELSE . ." OUT OF BOUNDS FOR ZP,Y ADDR MODE " ENDIF ;
+
+: MODE-ABS-X 6 @INDEX ;
+
+: MODE-ABS-Y 7 @INDEX ;
 
 ( CONSTANTS )
-( OPCODE ADDR MODES )
-( LDA )
-$A9 CONSTANT LDA-IMM $A5 CONSTANT LDA-ZP $B5 CONSTANT LDA-ZP,X $AD CONSTANT LDA-ABS
-$BD CONSTANT LDA-ABS,X $B9 CONSTANT LDA-ABS,Y $A1 CONSTANT LDA-IND,X $B1 CONSTANT LDA-IND,Y
+VARIABLE ADDR-MODE  \ set by words: (ACC # ,X ,Y (,X) (,Y)) and reset by all opcode words
+' MODE-DEFAULT ADDR-MODE ! 
+
+( OPCODE CONSTRUCTOR )
+: OPCODE   CREATE
+             ,  \ INDY
+             ,  \ INDX
+             ,  \ ABSY
+             ,  \ ABSX
+             ,  \ ABS
+             ,  \ ZPY
+             ,  \ ZPX
+             ,  \ ZP
+             ,  \ IMM
+             ,  \ ACC
+
+
+DOES> ADDR-MODE PERFORM  \ call function pointed to by ADDR-MODE,
+      ['] MODE-DEFAULT ADDR-MODE ! ;
+
+( OPCODES )
+$B1 $A1 $B9 $BD $AD 00 $B5 $A5 $A9 $00 OPCODE LDA
+: BRK $00 ; \ BRK simply pushes its byte (as do all opcodes which use only implied addressing)
+            \ and so its code $00 is repurposed in OPCODE-defined words to signify an invalid addr mode
+            
+: CLC $18 ;
+: CLD $D8 ;
+: CLI $58 ;
+: CLV $B8 ;
+: DEX $CA ;
+: DEY $88 ;
+: INX $E8 ;
+: INY $C8 ;
+: NOP $EA ;
+: PHA $48 ;
+: PHP $08 ;
+: PLA $68 ;
+: PLP $28 ;
+: RTI $40 ;
+: RTS $60 ;
+: SEC $38 ;
+: SED $F8 ;
+: SEI $78 ;
+: TAX $AA ;
+: TAY $A8 ;
+: TSX $BA ;
+: TXA $8A ;
+: TXS $9A ;
+: TYA $98 ;
 
 ( VARIABLES )
-VARIABLE ADDR-MODE \ set by words: (ACC # ,X ,Y (,X) (,Y)) and reset by all opcode words
-0 ADDR-MODE !
 
 ( IMPLICIT )          \ some modes don't require an associated word
-( ZERO PAGE )
-( ZERO PAGE X )
-( ZERO PAGE Y )
-( ABSOLUTE )
-( ABSOLUTE X )
-( ABSOLUTE Y )
 ( ACCUMULATOR )
   : ACC   1 ADDR-MODE ! ;
 ( IMMEDIATE )
-  : #     2 ADDR-MODE ! ;
+  : #     ['] MODE-IMM ADDR-MODE ! ;
+( ZERO PAGE )
+( ZERO PAGE X )
+( ZERO PAGE Y )
+( RELATIVE )
+( ABSOLUTE )
+( ABSOLUTE X )
+( ABSOLUTE Y )
 ( ,X )
-  : ,X    3 ADDR-MODE ! ;
+  : ,X    
+          DUP ?OPERAND-IN-ZP-RANGE
+          IF ['] MODE-ZP-X ADDR-MODE !
+          ELSE ['] MODE-ABS-X ADDR-MODE !
+          ENDIF ;
 ( ,Y )
-  : ,Y    4 ADDR-MODE ! ;
+  : ,Y    
+          DUP ?OPERAND-IN-ZP-RANGE
+          IF ['] MODE-ZP-Y ADDR-MODE !
+          ELSE ['] MODE-ABS-Y ADDR-MODE !
+          ENDIF ;
+
 ( INDEXED INDIRECT )
   : (,X)  5 ADDR-MODE ! ;
 ( INDIRECT INDEXED )
   : (,Y)  6 ADDR-MODE ! ;
-
-: ?<=$FF ( op -- op finrange ) DUP $FF <= ;
-: ?OPERAND-IN-ZP-RANGE   ?<=$FF ; \ distinguishes between zp and absolute addressing modes
-: ?OPERAND-IN-IMM-RANGE  ?<=$FF ; \ registers can only hold 8 bits
-
-( LDA )
-  : LDA   ADDR-MODE @
-          CASE
-            0 OF ?OPERAND-IN-ZP-RANGE IF LDA-ZP ELSE LDA-ABS ENDIF ENDOF \ needs abs range check <=$FFFF
-            1 OF ." INCOMPATIBLE ADDR MODE FOR LDA: ACC" ENDOF
-            2 OF ?OPERAND-IN-IMM-RANGE IF LDA-IMM ELSE . ." OUT OF RANGE FOR IMM ADDR MODE" ENDIF ENDOF
-            3 OF ?OPERAND-IN-ZP-RANGE IF LDA-ZP,X ELSE LDA-ABS,X ENDIF ENDOF \ also needs abs range check
-            4 OF ." INCOMPATIBLE ADDR MODE FOR LDA: ZP,Y" ENDOF
-            5 OF ?OPERAND-IN-ZP-RANGE IF LDA-IND,X ELSE . ." OUT OF RANGE FOR ZP ADDR MODE" ENDIF ENDOF
-            6 OF ?OPERAND-IN-ZP-RANGE IF LDA-IND,Y ELSE . ." OUT OF RANGE FOR ZP ADDR MODE" ENDIF ENDOF
-          ENDCASE
-          0 ADDR-MODE ! ;
